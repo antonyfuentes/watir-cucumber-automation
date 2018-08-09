d7a7b219be92226273cff760a193c208c7978bd2
Antony Fuentes <antonyfuentes@Anthonys-MacBook-Pro.local>
2018-08-09 02:19:30 -0600

    Fixing folder structure

--- step_definitions/home_page_steps.rb
+++
@@ -1,8 +0,0 @@
-
-When("I'm at InllMedia homepage") do
-  expect(site.home_page.home_page?).to be true
-end
-
-Then("I validate InAllMedia logo is present in the site") do
-  expect(site.home_page.logo_visible).to be true
-end
